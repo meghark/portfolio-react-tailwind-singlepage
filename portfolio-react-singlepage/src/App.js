e0651521e7e2566465271ca00f1b5d38d666af59
meghark <98137756+meghark@users.noreply.github.com>
2022-07-05 23:40:37 -0700

    Added About section

--- portfolio-react-singlepage/src/App.js
+++ portfolio-react-singlepage/src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-    
+    <div>
+      <main>
+        <About></About>
+      </main>
     </div>
   );
 }
---
+++ portfolio-react-singlepage/src/components/About/index.js
@@ -0,0 +1,22 @@
+import profilePic from "../../assets/images/profilepic.jpg";
+
+function About() {
+    return (
+      <section>
+        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            <h1 className="text-3xl font-bold text-gray-900">Megha Raj Kulathattil</h1>
+            <div className="ml-3 relative">
+                <img className="h-10 w-10 rounded-full" src={profilePic} alt="Profile picture"/>
+            </div>
+            <h2 className="mt-1 text-lg font-medium text-gray-900">Software Developer In Test</h2>
+          </div>
+          <p>
+          Over 10 years of experience as software engineer in multiple roles as a developer, SDET and team lead.
+           Interested in Software automated testing and committed to reducing manual effort in testing and improving
+            test coverage. Strengths in adaptability and meeting timelines.
+          </p>
+      </section>
+    );
+  }
+  
+  export default About;
