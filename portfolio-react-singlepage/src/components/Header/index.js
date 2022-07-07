d6ea1cf99dea4a13ab4274aacc4d8898ad4e007b
meghark <98137756+meghark@users.noreply.github.com>
2022-07-07 00:56:24 -0700

    Fix bug for navigation tabs focus state

--- portfolio-react-singlepage/src/components/Header/index.js
+++ portfolio-react-singlepage/src/components/Header/index.js
@@ -6,23 +6,24 @@ import { Link } from 'react-router-dom';
 function Header() {
     return (
       <header>
+
           <nav className="bg-gray-800">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex items-center justify-between h-16">
               <div className="flex items-center">                
                 <div className="hidden md:block">
                   <div className="ml-10 flex items-baseline space-x-4">
-          
-                    <Link to="/" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">About Me</Link>
+                    <p className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">Megha Kulathattil</p>
+                    <Link to="/" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium active:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 " aria-current="page">About Me</Link>
       
-                    <Link to="/portfolio"  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Portfolio</Link>
+                    <Link to="/portfolio"  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium active:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 ">Portfolio</Link>
       
-                    <Link to="/work"  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Work Experience</Link>
+                    <Link to="/work"  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium active:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 ">Work Experience</Link>
 
-                    <Link to="/resume"  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Resume</Link>
+                    <Link to="/resume"  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium active:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 ">Resume</Link>
                 
       
-                    <Link to="/contact"  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact Me</Link>
+                    <Link to="/contact"  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium active:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 ">Contact Me</Link>
                   </div>
                 </div>
               </div>             
